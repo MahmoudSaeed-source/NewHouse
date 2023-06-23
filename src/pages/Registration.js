a0a7ea90d08500c20bde0b7b69549a75e23b31e0
MAHMOUDSAEED <MahmoudSaid.wd@gmail.com>
2023-06-23 21:30:16 +0200

    form validation

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -1,7 +1,34 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import Login from './Login';
+import React,{useRef,useState,useEffect} from 'react'
+import axios from 'axios'
 const Registration = () => {
+  const ErrorMassageRef = useRef(null)
+  const passWordRef = useRef('')
+  const EmailREf = useRef('')
+  const ConfirmPasswordRed = useRef('')
+  const [userName,setUserName]=useState ('')
+  console.log(userName)
+  const [userNameValid,setUserNameValid]=useState (false)
+  const [passwordValid,setPasswordValid]=useState (false)
+  const [emailvalid,setEmailValid] = useState(false)
+  const nameRegex = /^[a-zA-Z_]+$/
+ 
+const checkData = (e)=>{
+  if(e.target.value == ""){
+    ErrorMassageRef.current.textContent   = 'userName is reqiure'
+  } 
+}
+
+  const handle_Data_Form =  async (e)   => {
+    e.preventDefault()
+    const GetData = new FormData(e.currentTarget)
+    const dataFormObject = Object.fromEntries(GetData)
+    try {
+      const res = await axios.post('http://localhost:5000/users',dataFormObject);
+    } catch(error) {
+      console.error(error);
+    }
+  }
+
   return (
     <main className='w-full h-screen flex items-center justify-center bg-hero-pattern'>
       <div className=' w-full h-full flex items-center justify-center flex-row' style={{ backgroundColor: '#333333ba' }} >
@@ -10,31 +37,25 @@ const Registration = () => {
           <h2 className='w-full h-10 flex justify-center items-center text-black font-body text-[30px] font-bold mb-2'>
             Sing Up
           </h2>
-          <form className='  full h-auto flex-col flex font-links relative'>
+          <form className='  full h-auto flex-col flex font-links relative' onSubmit={handle_Data_Form}>
             <div className='w-full h-auto  '>
-              <input className='w-full h-10 mt-2 rounded-md p-2 border-2 border-gray-100 outline-none	' type='text' placeholder='userName' name='username' />
+              <input className='w-full h-10 mt-2 rounded-md p-2 border-2 border-gray-100 outline-none	' autoComplete = 'none'  type='text' placeholder='userName'
+               name='username' onChange ={(e)=> setUserName(e.target.value)} onBlur = {checkData} onFocus = {handleFocus}/>
+               <p ref={ErrorMassageRef} className =' w-full text-[10px] text-red-600 font-body text-start'></p>
             </div>
             <div className='w-full h-auto  '>
-              <input className='w-full h-10 mt-2 rounded-md p-2 border-2 border-gray-100 outline-none	' type='email' placeholder='Email' name='Email' />
+              <input className='w-full h-10 mt-2 rounded-md p-2 border-2 border-gray-100 outline-none	' autoComplete='none'  ref={EmailREf} type='email' placeholder='Email' name='Email' />
             </div>
             <div className='w-full h-auto mt-2 '>
-              <input className='w-full h-10 mt-2 rounded-md p-2 border-2 border-gray-100 outline-none	' type='password' placeholder='Password' name='password ' />
+              <input className='w-full h-10 mt-2 rounded-md p-2 border-2 border-gray-100 outline-none	' autoComplete='none' ref={passWordRef} type='password' placeholder='Password' name='password ' />
             </div>
             <div className='w-full h-auto mt-2 '>
-              <input className='w-full h-10 mt-2 rounded-md p-2 border-2 border-gray-100 outline-none	' type='password' placeholder='ConfirmPassword' name='ConfirmPassword ' />
+              <input className='w-full h-10 mt-2 rounded-md p-2 border-2 border-gray-100 outline-none	' autoComplete='none' ref={ConfirmPasswordRed}  type='password' placeholder='ConfirmPassword' name='ConfirmPassword ' />
             </div>
             <div className='w-full h-[50px] flex justify-center items-center mt-4'>
               <button type='submit' className='w-[90%] h-[35px] rounded-md bg-blue-title font-body text-white text-center '>SingUp</button>
             </div>
-            <p className='w-full h-10 text-center mt-2 mb-4'>Already have an account ?  <span className='text-blue-title'><Link to='/' > Login</Link></span></p>
-            <div className='w-full h-10 relative'>
-              <hr className='mt-2' />
-              <p className='w-4 h-6 absolute top-[-5px] left-[50%] text-black  bg-white font-body font-bold'>or</p>
-            </div>
          
-            <div className='w-full h-10 border-2 border-gray-500 mt-6'>
-
-            </div>
           </form>
         </div>
     
